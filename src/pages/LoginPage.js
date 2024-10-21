c57e76944a926a1233700b4da14e558afd4cc6f7
Sunil Park <sunilpark1129@gmail.com>
2024-10-20 21:35:53 -0400

    first commit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../utils/api";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    try {
+      const response = await api.post("/user/login", { email, password });
+
+      if (response.status === 200) {
+        setUser(response.data.user);
+        sessionStorage.setItem("token", response.data.token);
+
+        // 토큰 값을 해더에 넣어서 GET을 해준다
+        api.defaults.headers["authorization"] = `Bearer ${response.data.token}`;
+
+        setError(null);
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
     <div className="display-center">
-      <Form className="login-box">
+      <Form className="login-box" onSubmit={submitHandler}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            autoComplete="off"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
         </Form.Group>
+        {error && <div className="red-error">{error}</div>}
         <div className="button-box">
           <Button type="submit" className="button-primary">
             Login
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,32 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import api from "../utils/api";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const secpassword = e.target.secpassword.value;
+
+    try {
+      if (password !== secpassword) {
+        throw new Error("패스워드가 일치하지 않습니다 다시 입력해주세요");
+      }
+      const response = await api.post("/user", { name, email, password });
+      if (response.status === 200) {
+        setError(null);
+        navigate("/login");
+      } else {
+        throw new Error(response.data.error);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
     <div className="display-center">
-      <Form className="login-box">
+      <Form className="login-box" onSubmit={submitHandler}>
         <h1>회원가입</h1>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control
+            type="string"
+            name="name"
+            placeholder="Name"
+            autoComplete="off"
+          />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            autoComplete="off"
+          />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Control
+            type="password"
+            name="secpassword"
+            placeholder="re-enter the password"
+          />
         </Form.Group>
-
+        {error && <div className="red-error">{error}</div>}
         <Button className="button-primary" type="submit">
           회원가입
         </Button>
